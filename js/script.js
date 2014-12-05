8dc100363f4a49b74f7988f370ddea8ee9a367d7
Dushman <ebanniynasos@gmail.com>
2014-12-05 11:34:38 +0200

    4

--- js/script.js
+++ js/script.js
@@ -44,21 +44,21 @@ function pageInit(){
 
     //Form---------------------------------------------------
 
-    $('#name').on('input', function() {
+    $('#order-name').on('input', function() {
         var input = $(this);
         var is_name = input.val();
         if (is_name) {input.removeClass('error').addClass('valid');}
         else {input.removeClass('valid').addClass('error');}
     });
 
-    $('#type').on('input', function() {
+    $('#order-type').on('input', function() {
         var input = $(this);
         var is_name = input.val();
         if (is_name) {input.removeClass('error').addClass('valid');}
         else {input.removeClass('valid').addClass('error');}
     });
 
-    $('#phone').on('input', function() {
+    $('#order-phone').on('input', function() {
         var input = $(this);
         var re = /^[0-9\-\+]{8,15}$/;
         var is_phone = re.test(input.val());
@@ -66,7 +66,7 @@ function pageInit(){
         else {input.removeClass('valid').addClass('error');}
     });
 
-    $('#email').on('input', function() {
+    $('#order-email').on('input', function() {
         var input = $(this);
         var re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
         var is_email = re.test(input.val());
@@ -74,15 +74,15 @@ function pageInit(){
         else {input.removeClass('valid').addClass('error');}
     });
             
-    $('#message').keyup(function(event) {
+    $('#order-message').keyup(function(event) {
         var input = $(this);
         var message = $(this).val();
         if (message) {input.removeClass('error').addClass('valid');}
         else {input.removeClass('valid').addClass('error');}   
     });
         
-    $('.send-order-btn').click(function(e){
-        var form_data = $('.order-form').serializeArray();
+    $('.send-mf-btn').click(function(e){
+        var form_data = $('.main-form').serializeArray();
         var error_free = true;
         for (var input in form_data){
             var element = $('#order-' + form_data[input]['name']);
@@ -94,8 +94,8 @@ function pageInit(){
             e.preventDefault(); 
         } else{
             e.preventDefault(); 
-            $('.order-block').hide();
-            $('.success-block').fadeIn('slow');
+            //$('.order-block').hide();
+            //$('.success-block').fadeIn('slow');
         }
     });
 
