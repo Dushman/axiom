29d61ab334c5f0d8b093f57dc3fe380304316097
Dushman <ebanniynasos@gmail.com>
2014-12-05 11:21:37 +0200

    1
    1

---
+++ js/libs/scroll.js
@@ -0,0 +1,349 @@
+/**
+ * Extend jquery with a scrollspy plugin.
+ * This watches the window scroll and fires events when elements are scrolled into viewport.
+ *
+ * throttle() and getTime() taken from Underscore.js
+ * https://github.com/jashkenas/underscore
+ *
+ * @author Copyright 2013 John Smart
+ * @license https://raw.github.com/thesmart/jquery-scrollspy/master/LICENSE
+ * @see https://github.com/thesmart
+ * @version 0.1.2
+ */
+(function($) {
+
+	var jWindow = $(window);
+	var elements = [];
+	var elementsInView = [];
+	var isSpying = false;
+	var ticks = 0;
+
+	/**
+	 * Find elements that are within the boundary
+	 * @param {number} top
+	 * @param {number} right
+	 * @param {number} bottom
+	 * @param {number} left
+	 * @return {jQuery}		A collection of elements
+	 */
+	function findElements(top, right, bottom, left) {
+		var hits = $();
+		$.each(elements, function(i, element) {
+			var elTop = element.offset().top,
+				elLeft = element.offset().left,
+				elRight = elLeft + element.width(),
+				elBottom = elTop + element.height();
+
+			var isIntersect = !(elLeft > right ||
+				elRight < left ||
+				elTop > bottom ||
+				elBottom < top);
+
+			if (isIntersect) {
+				hits.push(element);
+			}
+		});
+
+		return hits;
+	}
+
+	/**
+	 * Called when the user scrolls the window
+	 */
+	function onScroll() {
+		// unique tick id
+		++ticks;
+
+		// viewport rectangle
+		var top = jWindow.scrollTop(),
+			left = jWindow.scrollLeft(),
+			right = left + jWindow.width(),
+			bottom = top + jWindow.height();
+
+		// determine which elements are in view
+		var intersections = findElements(top, right, bottom, left);
+		$.each(intersections, function(i, element) {
+			var lastTick = element.data('scrollSpy:ticks');
+			if (typeof lastTick != 'number') {
+				// entered into view
+				element.triggerHandler('scrollSpy:enter');
+			}
+
+			// update tick id
+			element.data('scrollSpy:ticks', ticks);
+		});
+
+		// determine which elements are no longer in view
+		$.each(elementsInView, function(i, element) {
+			var lastTick = element.data('scrollSpy:ticks');
+			if (typeof lastTick == 'number' && lastTick !== ticks) {
+				// exited from view
+				element.triggerHandler('scrollSpy:exit');
+				element.data('scrollSpy:ticks', null);
+			}
+		});
+
+		// remember elements in view for next tick
+		elementsInView = intersections;
+	}
+
+	/**
+	 * Called when window is resized
+	*/
+	function onWinSize() {
+		jWindow.trigger('scrollSpy:winSize');
+	}
+
+	/**
+	 * Get time in ms
+   * @license https://raw.github.com/jashkenas/underscore/master/LICENSE
+	 * @type {function}
+	 * @return {number}
+	 */
+	var getTime = (Date.now || function () {
+		return new Date().getTime();
+	});
+
+	/**
+	 * Returns a function, that, when invoked, will only be triggered at most once
+	 * during a given window of time. Normally, the throttled function will run
+	 * as much as it can, without ever going more than once per `wait` duration;
+	 * but if you'd like to disable the execution on the leading edge, pass
+	 * `{leading: false}`. To disable execution on the trailing edge, ditto.
+	 * @license https://raw.github.com/jashkenas/underscore/master/LICENSE
+	 * @param {function} func
+	 * @param {number} wait
+	 * @param {Object=} options
+	 * @returns {Function}
+	 */
+	function throttle(func, wait, options) {
+		var context, args, result;
+		var timeout = null;
+		var previous = 0;
+		options || (options = {});
+		var later = function () {
+			previous = options.leading === false ? 0 : getTime();
+			timeout = null;
+			result = func.apply(context, args);
+			context = args = null;
+		};
+		return function () {
+			var now = getTime();
+			if (!previous && options.leading === false) previous = now;
+			var remaining = wait - (now - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0) {
+				clearTimeout(timeout);
+				timeout = null;
+				previous = now;
+				result = func.apply(context, args);
+				context = args = null;
+			} else if (!timeout && options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
+			}
+			return result;
+		};
+	};
+
+	/**
+	 * Enables ScrollSpy using a selector
+	 * @param {jQuery|string} selector		The elements collection, or a selector
+	 * @param {Object=} options						Optional. Set { throttle: number } to change scrollspy throttling. Default: 100 ms
+	 * @returns {jQuery}
+	 */
+	$.scrollSpy = function(selector, options) {
+		selector = $(selector);
+		selector.each(function(i, element) {
+			elements.push($(element));
+		});
+		options = options || {
+			throttle: 100
+		};
+
+		if (!isSpying) {
+			jWindow.on('scroll', throttle(onScroll, options.throttle || 100));
+			jWindow.on('resize', throttle(onScroll, options.throttle || 100));
+			isSpying = true;
+
+			// perform a scan once, after current execution context, and after dom is ready
+			setTimeout(function() {
+				$(document).ready(onScroll);
+			}, 0);
+		}
+
+		return selector;
+	};
+
+	/**
+	 * Listen for window resize events
+	 * @param {Object=} options						Optional. Set { throttle: number } to change throttling. Default: 100 ms
+	 * @returns {jQuery}		$(window)
+	 */
+	$.winSizeSpy = function(options) {
+		$.winSizeSpy = function() { return jWindow; }; // lock from multiple calls
+		options = options || {
+			throttle: 100
+		};
+		return jWindow.on('resize', throttle(onWinSize, options.throttle || 100));
+	};
+
+	/**
+	 * Enables ScrollSpy on a collection of elements
+	 * e.g. $('.scrollSpy').scrollSpy()
+	 * @param {Object=} options						Optional. Set { throttle: number } to change scrollspy throttling
+	 * @returns {jQuery}
+	 */
+	$.fn.scrollSpy = function(options) {
+		return $.scrollSpy($(this), options);
+	};
+
+})(jQuery);
+
+/* ========================================================================
+ * Bootstrap: scrollspy.js v3.0.3
+ * http://getbootstrap.com/javascript/#scrollspy
+ * ========================================================================
+ * Copyright 2013 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+
++function ($) { 'use strict';
+
+  // SCROLLSPY CLASS DEFINITION
+  // ==========================
+
+  function ScrollSpy(element, options) {
+    var href
+    var process  = $.proxy(this.process, this)
+
+    this.$element       = $(element).is('body') ? $(window) : $(element)
+    this.$body          = $('body')
+    this.$scrollElement = this.$element.on('scroll.bs.scroll-spy.data-api', process)
+    this.options        = $.extend({}, ScrollSpy.DEFAULTS, options)
+    this.selector       = (this.options.target
+      || ((href = $(element).attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
+      || '') + ' .nav li > a'
+    this.offsets        = $([])
+    this.targets        = $([])
+    this.activeTarget   = null
+
+    this.refresh()
+    this.process()
+  }
+
+  ScrollSpy.DEFAULTS = {
+    offset: 10
+  }
+
+  ScrollSpy.prototype.refresh = function () {
+    var offsetMethod = this.$element[0] == window ? 'offset' : 'position'
+
+    this.offsets = $([])
+    this.targets = $([])
+
+    var self     = this
+    var $targets = this.$body
+      .find(this.selector)
+      .map(function () {
+        var $el   = $(this)
+        var href  = $el.data('target') || $el.attr('href')
+        var $href = /^#./.test(href) && $(href)
+
+        return ($href
+          && $href.length
+          && [[ $href[offsetMethod]().top + (!$.isWindow(self.$scrollElement.get(0)) && self.$scrollElement.scrollTop()), href ]]) || null
+      })
+      .sort(function (a, b) { return a[0] - b[0] })
+      .each(function () {
+        self.offsets.push(this[0])
+        self.targets.push(this[1])
+      })
+  }
+
+  ScrollSpy.prototype.process = function () {
+    var scrollTop    = this.$scrollElement.scrollTop() + this.options.offset
+    var scrollHeight = this.$scrollElement[0].scrollHeight || this.$body[0].scrollHeight
+    var maxScroll    = scrollHeight - this.$scrollElement.height()
+    var offsets      = this.offsets
+    var targets      = this.targets
+    var activeTarget = this.activeTarget
+    var i
+
+    if (scrollTop >= maxScroll) {
+      return activeTarget != (i = targets.last()[0]) && this.activate(i)
+    }
+
+    for (i = offsets.length; i--;) {
+      activeTarget != targets[i]
+        && scrollTop >= offsets[i]
+        && (!offsets[i + 1] || scrollTop <= offsets[i + 1])
+        && this.activate( targets[i] )
+    }
+  }
+
+  ScrollSpy.prototype.activate = function (target) {
+    this.activeTarget = target
+
+    $(this.selector)
+      .parentsUntil(this.options.target, '.active')
+      .removeClass('active')
+
+    var selector = this.selector +
+        '[data-target="' + target + '"],' +
+        this.selector + '[href="' + target + '"]'
+
+    var active = $(selector)
+      .parents('li')
+      .addClass('active')
+
+    if (active.parent('.dropdown-menu').length) {
+      active = active
+        .closest('li.dropdown')
+        .addClass('active')
+    }
+
+    active.trigger('activate.bs.scrollspy')
+  }
+
+
+  // SCROLLSPY PLUGIN DEFINITION
+  // ===========================
+
+  var old = $.fn.scrollspy
+
+  $.fn.scrollspy = function (option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('bs.scrollspy')
+      var options = typeof option == 'object' && option
+
+      if (!data) $this.data('bs.scrollspy', (data = new ScrollSpy(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  $.fn.scrollspy.Constructor = ScrollSpy
+
+
+  // SCROLLSPY NO CONFLICT
+  // =====================
+
+  $.fn.scrollspy.noConflict = function () {
+    $.fn.scrollspy = old
+    return this
+  }
+
+
+  // SCROLLSPY DATA-API
+  // ==================
+
+  $(window).on('load', function () {
+    $('[data-spy="scroll"]').each(function () {
+      var $spy = $(this)
+      $spy.scrollspy($spy.data())
+    })
+  })
+
+}(jQuery);
---
+++ js/script.js
@@ -0,0 +1,107 @@
+$(pageInit);
+
+function pageInit(){
+
+	$('.top-logo').click(function(){window.location.reload()});
+
+    var $page = $('html,body'),
+        $body = $('body');
+
+    $body.bsScrollSpy;
+
+    function scrollToPage(target) {
+        var y = 0;
+        if (target && $(target).length) {
+            y = $(target).offset().top - 100;
+        }
+        $page.animate({scrollTop: y}, 'slow', 'swing');
+    }
+
+    $body.on('click', '.top-menu a', function(e){
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+     
+    $body.on('click', '.top-menu a', function(e){
+        e.preventDefault();
+        scrollToPage($(this).attr('href'));
+    });
+
+    $(window).scroll(function () { 
+        if ($(this).scrollTop() > 350) {$('.top-menu').addClass('fixed');}
+        else {$('.top-menu').removeClass('fixed');}   
+    });
+
+    $('#wrapper .section').scrollSpy();
+
+	$('#wrapper .section').on('scrollSpy:enter', function(){
+		$(this).addClass('current');
+	});
+
+	$('#wrapper .section').on('scrollSpy:exit', function(){
+		$(this).removeClass('current');
+	});
+
+    //Form---------------------------------------------------
+
+    $('input')attr('data-name').on('input', function() {
+        var input = $(this);
+        var is_name = input.val();
+        if (is_name) {input.removeClass('error').addClass('valid');}
+        else {input.removeClass('valid').addClass('error');}
+    });
+
+    $('#order-last-name').on('input', function() {
+        var input = $(this);
+        var is_name = input.val();
+        if (is_name) {input.removeClass('error').addClass('valid');}
+        else {input.removeClass('valid').addClass('error');}
+    });
+
+    $('#order-phone').on('input', function() {
+        var input = $(this);
+        var re = /^[0-9\-\+]{8,15}$/;
+        var is_phone = re.test(input.val());
+        if (is_phone) {input.removeClass('error').addClass('valid');}
+        else {input.removeClass('valid').addClass('error');}
+    });
+
+    $('#order-email').on('input', function() {
+        var input = $(this);
+        var re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+        var is_email = re.test(input.val());
+        if (is_email) {input.removeClass('error').addClass('valid');}
+        else {input.removeClass('valid').addClass('error');}
+    });
+            
+    $('#order-message').keyup(function(event) {
+        var input = $(this);
+        var message = $(this).val();
+        if (message) {input.removeClass('error').addClass('valid');}
+        else {input.removeClass('valid').addClass('error');}   
+    });
+        
+    $('.send-order-btn').click(function(e){
+        var form_data = $('.order-form').serializeArray();
+        var error_free = true;
+        for (var input in form_data){
+            var element = $('#order-' + form_data[input]['name']);
+            var valid = element.hasClass('valid');
+            if (!valid){element.addClass('error'); error_free = false;}
+            else {element.removeClass('error');}
+        }
+        if (!error_free){
+            e.preventDefault(); 
+        } else{
+            e.preventDefault(); 
+            $('.order-block').hide();
+            $('.success-block').fadeIn('slow');
+        }
+    });
+
+
+
+    
+
+		
+}
